d1dbdded49848964f6af6934e011e351c85bdad3
cricritoia <toiacristiandaniel@gmail.com>
2024-05-26 01:16:14 -0300

    actualizando estilos de index y contacto

---
+++ js/validaciones.js
@@ -0,0 +1,11 @@
+
+const form = document.getElementById('formulario');
+const nombreInput = document.getElementById('nombre');
+const emailInput = document.getElementById('email');
+const telefonoInput = document.getElementById('telefono');
+const mensajeInput = document.getElementById('mensaje');
+const selectInput = document.getElementById('opciones');
+const precioInput = document.getElementById('precio');
+const archivoInput = document.getElementById('archivo');
+const contactoTelefonoRadioBtn = document.getElementById('contactar-telefono');
+const contactoEmailRadioBtn = document.getElementById('contactar-email');
